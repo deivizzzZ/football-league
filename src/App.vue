61e913a59829469abb27bd1b1a38985d7df0e0da
David Alvarez <marava.jr@gmail.com>
2022-02-16 11:23:39 +0100

    Add spanish teams module to export

--- src/App.vue
+++ src/App.vue
@@ -30,36 +30,10 @@
 </style>
 
 <script>
-import Team from "@/js/Team.js";
+import SPAIN_ONE from "@/js/spainOne.js";
 export default {
-  data() {
-    return {
-      teams: [
-        new Team("Alavés", "/img/spain1/alaves.png"),
-        new Team("Athletic", "/img/spain1/athletic.png"),
-        new Team("Atlético Madrid", "/img/spain1/atletico.png"),
-        new Team("Barcelona", "/img/spain1/barcelona.png"),
-        new Team("Betis", "/img/spain1/betis.png"),
-        new Team("Cádiz", "/img/spain1/cadiz.png"),
-        new Team("Celta de Vigo", "/img/spain1/celta.png"),
-        new Team("Elche C.F.", "/img/spain1/elche.png"),
-        new Team("RCD Espanyol", "/img/spain1/espanyol.png"),
-        new Team("Getafe", "/img/spain1/getafe.png"),
-        new Team("Granada", "/img/spain1/granada.png"),
-        new Team("Levante", "/img/spain1/levante.png"),
-        new Team("Real Madrid", "/img/spain1/madrid.png"),
-        new Team("R.C.D. Mallorca", "/img/spain1/mallorca.png"),
-        new Team("Osasuna", "/img/spain1/osasuna.png"),
-        new Team("Rayo Vallecano", "/img/spain1/rayo.png"),
-        new Team("Real Sociedad", "/img/spain1/real.png"),
-        new Team("Sevilla", "/img/spain1/sevilla.png"),
-        new Team("Valencia C.F.", "/img/spain1/valencia.png"),
-        new Team("Villarreal", "/img/spain1/villarreal.png")
-      ]
-    };
-  },
   mounted() {
-    this.$store.dispatch("addTeams", this.teams);
+    this.$store.dispatch("addTeams", SPAIN_ONE);
   }
 };
 </script>
