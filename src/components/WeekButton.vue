258ce957d6d5d52110fa4553291af071a43bb204
David Alvarez <marava.jr@gmail.com>
2022-02-18 20:12:09 +0100

    Simulation finished at the bottom of calendar

--- src/components/WeekButton.vue
+++ src/components/WeekButton.vue
@@ -1,15 +1,23 @@
 <template>
   <button v-if="!this.$store.state.simulationStarted" @click="startSimulation">COMENZAR</button>
-  <button v-if="this.$store.state.simulationStarted" @click="showWeek">SIGUIENTE</button>
+  <button v-if="running" @click="showWeek">SIGUIENTE</button>
+  <button v-if="finished">FINALIZAR</button>
 </template>
 
 <script>
 export default {
   name: "WeekButton",
+  data() {
+    return {
+      running: this.$store.state.simulationStarted,
+      finished: false
+    };
+  },
   methods: {
     startSimulation() {
       this.showWeek();
       this.$store.dispatch("startSimulation");
+      this.running = true;
     },
     showWeek() {
       const matches = this.$store.state.calendar[this.$store.state.weekCounter];
@@ -20,6 +28,10 @@ export default {
       });
       this.$store.dispatch("showMatches", matches);
       this.$store.dispatch("nextWeek");
+      if (this.$store.state.weekCounter === this.$store.state.calendar.length) {
+        this.running = false;
+        this.finished = true;
+      }
     }
   }
 };
